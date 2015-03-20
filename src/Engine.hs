f8d34c39630c1e54f47aff4c2ed9cc4d95eb4781
Thomas Eding <thomaseding@gmail.com>
2015-03-20 00:18:55 -0700

    more scaffolding

--- src/Engine.hs
+++ src/Engine.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 
 module Engine where
@@ -9,6 +13,10 @@ module Engine where
 --------------------------------------------------------------------------------
 
 
+import Control.Applicative
+import Control.Monad.Prompt
+import Control.Monad.Random
+import Control.Monad.State
 import Names
 
 
@@ -37,6 +45,7 @@ newtype Health = Health Int
 
 data Effect :: * where
     Effect :: Effect
+    --WithElects :: [Elect] -> [Effect] -> Effect
     deriving (Show, Eq, Ord)
 
 
@@ -77,7 +86,7 @@ data DeckMinion = DeckMinion {
 
 
 data HandMinion = HandMinion {
-    --_handMinionEffects :: [HandEffect]  -- Think Bolivar
+    --_handMinionEffects :: [HandEffect]  -- Think Bolvar
     _handMinion :: Minion
 } deriving (Show, Eq, Ord)
 
@@ -90,7 +99,6 @@ data HeroPower = HeroPower {
 
 data Hero = Hero {
     _heroAttack :: Attack,
-    _heroArmor :: Armor,
     _heroHealth :: Health,
     _heroPower :: HeroPower,
     _heroName :: HeroName
@@ -99,13 +107,8 @@ data Hero = Hero {
 
 data BoardHero = BoardHero {
     _boardHeroCurrHealth :: Health,
+    _boardHeroArmor :: Armor,
     _boardHero :: Hero
-} deriving (Show, Eq, Ord)
-
-
-data GameState = GameState {
-    _turn :: Turn,
-    _players :: [Player]
 } deriving (Show, Eq, Ord)
 
 
@@ -139,20 +142,68 @@ data Player = Player {
 } deriving (Show, Eq, Ord)
 
 
+data GameState = GameState {
+    _turn :: Turn,
+    _players :: [Player]
+} deriving (Show, Eq, Ord)
 
 
+data HearthPrompt :: * -> * where
+    ShuffleDeck :: Deck -> HearthPrompt Deck
+deriving instance Show (HearthPrompt a)
+deriving instance Eq (HearthPrompt a)
+deriving instance Ord (HearthPrompt a)
 
 
+newtype Hearth m a = Hearth {
+    unHearth :: StateT GameState m a
+} deriving (Functor, Applicative, Monad, MonadState GameState, MonadIO, MonadTrans, MonadRandom)
 
 
+type HearthMonad m = MonadPrompt HearthPrompt m
 
 
+instance (HearthMonad m) => MonadPrompt HearthPrompt (Hearth m) where
+    prompt = lift . prompt
 
 
+data GameResult :: * where
+    GameResult :: GameResult
+    deriving (Show, Eq, Ord)
 
 
+data PlayerData = PlayerData Hero Deck
+    deriving (Show, Eq, Ord)
 
 
+runHearth :: (HearthMonad m) => [PlayerData] -> m GameResult
+runHearth = evalStateT (unHearth runGame) . mkGameState
+
+
+mkGameState :: [PlayerData] -> GameState
+mkGameState playerData = GameState {
+    _turn = 1, 
+    _players = map mkPlayer playerData }
+
+
+mkPlayer :: PlayerData -> Player
+mkPlayer (PlayerData hero deck) = Player {
+    _playerDeck = deck,
+    _playerHand = Hand [],
+    _playerMinions = [],
+    _playerHero = mkBoardHero hero }
+
+
+mkBoardHero :: Hero -> BoardHero
+mkBoardHero hero = BoardHero {
+    _boardHeroCurrHealth = _heroHealth hero,
+    _boardHeroArmor = 0,
+    _boardHero = hero }
+
+
+runGame :: (HearthMonad m) => Hearth m GameResult
+runGame = do
+    return GameResult
 
 
 
