f96d7c7a2f44c521220acdddea4d1e67bf2e45c9
Thomas Eding <thomaseding@gmail.com>
2015-03-23 23:37:46 -0700

    Rest of (use) to (view).

--- src/Control/Lens/Helper.hs
+++ src/Control/Lens/Helper.hs
@@ -5,20 +5,20 @@ module Control.Lens.Helper where
 
 
 import Control.Lens
-import Control.Monad.State
+import Control.Monad.Reader
 import Data.Monoid
 
 
 --------------------------------------------------------------------------------
 
 
-toListOfM :: (MonadState s m) => Getting (Endo [a]) s a -> m [a]
-toListOfM lens = gets $ toListOf lens
+toListOfM :: (MonadReader s m) => Getting (Endo [a]) s a -> m [a]
+toListOfM lens = asks $ toListOf lens
 
 
 infixl 1 >>=.
-(>>=.) :: MonadState s m => Getting a s a -> (a -> m b) -> m b
-lens >>=. f = use lens >>= f
+(>>=.) :: (MonadReader s m) => Getting a s a -> (a -> m b) -> m b
+lens >>=. f = view lens >>= f
 
 
 
